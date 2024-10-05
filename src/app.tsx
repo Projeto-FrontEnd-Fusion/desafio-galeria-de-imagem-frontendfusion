bf1773ad25b65c7ae7cdbba61c34d35532041b5c
Daniel Levi <daniellevi000@gmail.com>
2024-10-05 10:17:05 -0300

    chore: add typescript with tailwindcss

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+
+export  function App() {
+
+  return (
+    <>
+      <main className="flex items-center justify-center">
+        <h1 className="text-3xl text-green-600">Seja bem vindo! :)</h1>
+        <p>Desafio de Galeria de Imagens - Processo Seletivo Frontend Fusion</p>
+      </main>
+    </>
+  );
+}
+
