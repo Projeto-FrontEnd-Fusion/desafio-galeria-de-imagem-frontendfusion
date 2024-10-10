c5705e1b4da7ce9f57c7212b119381bca757cb14
Daniel Levi <daniellevi000@gmail.com>
2024-10-10 09:47:38 -0300

    chore: layz load and compression

--- src/components/photo-gallery.tsx
+++ src/components/photo-gallery.tsx
@@ -2,6 +2,7 @@ import * as Dialog from "@radix-ui/react-dialog";
 import type { photo } from "../@types/photo"
 import { SkeletonPhoto } from "./skeleton"
 import { PhotoModal } from "./photo-modal";
+import LazyLoad from 'react-lazyload';
 
 interface PhotoGalleryProps {
   photos: photo[]
@@ -25,14 +26,19 @@ export function PhotoGallery({ error,photos ,isLoading} : PhotoGalleryProps) {
         { photos && photos.map(photo => (
            <Dialog.Root key={photo.id}>
             <Dialog.Trigger>
+            <LazyLoad height={200} offset={100}>
               <img 
                 src={photo.download_url} 
+                srcSet={`${photo.download_url}?w=400 400w, ${photo.download_url}?w=800 800w`} 
+                sizes="(max-width: 600px) 400px, 800px"
                 width={80} 
                 height={80} 
-                className="border bg-gray-300 h-20 w-full sm:w-full sm:h-56 md:w-full md:h-80  rounded-lg object-cover lg:w-full lg:h-80 hover:scale-110 transition-all"
+                className="border bg-gray-300 h-20 w-full sm:w-full sm:h-56 md:w-full md:h-80 rounded-lg object-cover lg:w-full lg:h-80 md:hover:scale-110 transition-all"
                 alt={`Foto de autor ${photo.author}`}
                 loading="lazy"
               />
+            </LazyLoad>
+
             </Dialog.Trigger>
             <Dialog.Portal>
               <Dialog.Overlay className='inset-0 fixed bg-black/50'/> 
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,41 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import type { UserConfig } from 'vite'
-import type { InlineConfig } from 'vitest'
+import type { UserConfig } from 'vite';
+import type { InlineConfig } from 'vitest';
+import compression from 'vite-plugin-compression';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), 
+    compression(),  // Atualize aqui
+    compression({
+      verbose: true,
+      disable: false,
+      threshold: 10240,
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
+    ViteImageOptimizer({
+      includePublic: true,  // otimize imagens na pasta public
+      png: {
+        quality: 80,
+      },
+      jpeg: {
+        quality: 80,
+      },
+      webp: {
+        quality: 80,
+      },
+    }),
+    visualizer({
+      open: true, // Para abrir automaticamente o relatório após o build
+      filename: 'dist/stats.html', // Nome do arquivo de saída
+      template: 'sunburst', // Voc // abrir automaticamente após a construção
+    }),
+  ],
   test: {
     globals: true,
     setupFiles: ['./test/setup.ts'],
@@ -13,4 +43,4 @@ export default defineConfig({
   },
 } as UserConfig & {
   test: InlineConfig
-})
+});
